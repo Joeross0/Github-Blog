188076f471b8716e0a9a8bd108e2398426ea8e73
Joseph Ross <thattrollagen@gmail.com>
2021-12-11 23:45:36 -0600

    ygjh

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+//Cloud scroll
+let clouds = document.getElementsByClassName("cloud")[0]
+let oldscroll = 0;
+let scrollfloat = 0;
+
+
+jQuery(document).ready(function(){
+
+  window.addEventListener("scroll", (event) => {
+    scrollfloat += this.scrollY;
+    console.log(scrollfloat-oldscroll);
+    oldscroll = scrollfloat
+    $(".cloud").style.transform =  "translate(-100%, -100%);";
+  });
+  
+  
+});
+
+//About me
 var acc = document.getElementsByClassName("accordion");
 var i;
 
